66a201885c14d30783b39cd60adc3f108a271d58
Geoffrey Martin-Noble <Geoffrey.Martin-Noble@amd.com>
2025-12-19 00:14:38 +0000

    Remove torch.jit.script
    torch.jit.script blocks cause graph breaks under torch.compile and
    mixing two different compilation options seems a bit like asking for
    trouble. It would be possible to write a decorator that lazily invokes
    torch.jit.script (which itself is lazy) if not under torch.compile, but
    that would be pretty complicated. torch.jit.script is deprecated and the
    point of this work is to show improvements with torch.compile, so we'll
    take the performance hit in the non-compiled case.

--- se3_transformer/model/layers/norm.py
+++ se3_transformer/model/layers/norm.py
@@ -32,11 +32,9 @@ from se3_transformer.model.profiling import maybe_nvtx_range
 from se3_transformer.model.fiber import Fiber
 
 
-@torch.jit.script
 def clamped_norm(x, clamp: float):
     return x.norm(p=2, dim=-1, keepdim=True).clamp(min=clamp)
 
-@torch.jit.script
 def rescale(x, norm, new_norm):
     return x / norm * new_norm
 
