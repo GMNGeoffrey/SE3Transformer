7e7ade9200616dee09dbc7de1d06e9c846e6c024
Geoffrey Martin-Noble <Geoffrey.Martin-Noble@amd.com>
2025-12-19 00:17:19 +0000

    Wrapper for `nvtx.range` that noops under compilation
    `nvtx.range` unfortunately forces a graph break in the compilation
    graph, so we noop it when under `torch.compile`.

---
+++ se3_transformer/model/profiling.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+# SPDX-FileCopyrightText: Copyright (c) 2021-2022 NVIDIA CORPORATION & AFFILIATES
+# SPDX-License-Identifier: MIT
+
+import contextlib
+
+import torch
+
+
+def maybe_nvtx_range(msg, *args, **kwargs):
+    """Creates an NVTX range context if not compiling, but skips it under compilation.
+
+    nvtx.range causes a graph break under torch.compile, so we disable it.
+    """
+    if torch.compiler.is_compiling():
+        return contextlib.nullcontext()
+    else:
+        return torch.cuda.nvtx.range(msg, *args, **kwargs)
